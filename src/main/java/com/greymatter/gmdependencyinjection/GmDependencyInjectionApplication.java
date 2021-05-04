ecf03608ca1921ffb610cc914ca894a11cf282d0
JORGE <jcgarciamed@gmail.com>
2021-05-04 16:11:25 -0500

    initial commit

---
+++ src/main/java/com/greymatter/gmdependencyinjection/GmDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.greymatter.gmdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmDependencyInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmDependencyInjectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greymatter/gmdependencyinjection/GmDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greymatter.gmdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmDependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
