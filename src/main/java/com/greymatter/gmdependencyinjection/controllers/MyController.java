4db590d9746cf53ab500a0e7533ef02bbf557409
JORGE <jcgarciamed@gmail.com>
2021-05-04 19:37:15 -0500

    Adding Primay Bean

--- src/main/java/com/greymatter/gmdependencyinjection/controllers/MyController.java
+++ src/main/java/com/greymatter/gmdependencyinjection/controllers/MyController.java
@@ -1,12 +1,19 @@
 package com.greymatter.gmdependencyinjection.controllers;
 
+import com.greymatter.gmdependencyinjection.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
         System.out.println("Hello World!!!!");
-        return "hi folks!";
+        return greetingService.sayGreeting();
     }
 }
